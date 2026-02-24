11267d8fb711bf384387402947e29d0686f77ccb
jhinlang <julienhinlang@gmail.com>
2026-02-24 13:56:13 +0000

    feat: ajouter la classe Review avec gestion des attributs et validation

---
+++ part2/hbnb/app/models/review.py
@@ -0,0 +1,57 @@
+from basemodel import BaseModel
+
+class Review(BaseModel):
+    def __init__(self, text="", rating=0, place="", user=""):
+        super().__init__()
+        self.__text = text
+        self.__rating = rating
+        self.__place = place
+        self.__user = user
+
+    @property
+    def text(self):
+        """Gets the text of the review"""
+        return self.__text
+
+    @property
+    def rating(self):
+        """Gets the rating of the review"""
+        return self.__rating
+    
+    @property
+    def place(self):
+        """Gets the place of the review"""
+        return self.__place
+    
+    @property
+    def user(self):
+        """Gets the user of the review"""
+        return self.__user
+
+    @text.setter
+    def text(self, value):
+        """information must be checked"""
+        if not isinstance(value, str) or value == "":
+            raise ValueError("The text must be a string")
+        self.__text = value
+
+    @rating.setter
+    def rating(self, value):
+        """information must be checked"""
+        if not isinstance(value, int):
+            raise ValueError("The rating must be an integer")
+        if value < 0:
+            raise ValueError("The rating must be positive")
+        if value > 5:
+            raise ValueError("The rating must be less than or equal to 5")
+        self.__rating = value
+
+    @place.setter
+    def place(self, value):
+        """information must be checked"""
+        self.__place = value
+
+    @user.setter
+    def user(self, value):
+        """information must be checked"""
+        self.__user = value
