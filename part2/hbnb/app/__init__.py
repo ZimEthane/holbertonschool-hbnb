aecb4f2415fca51da64a5beda070441e47f6fc15
Ethane Zimmermann <zepsydu82@hotmail.com>
2026-03-03 16:21:27 +0000

    feat : add venv and update url

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -7,12 +7,12 @@ from app.api.v1.places import api as places_ns
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
 
     # Register the users namespace
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(users_ns, path='/users')
+    api.add_namespace(amenities_ns, path='/amenities')
+    api.add_namespace(places_ns, path='/places')
+    api.add_namespace(reviews_ns, path='/reviews')
     
     return app
